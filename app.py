66eb8bb28f34388d665053b0fe855af7fa30f073
kakiyamaaaa <kainan2000@hotmail.com>
2026-03-01 21:07:13 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 import random
 
 app = Flask(__name__)
@@ -30,4 +31,4 @@ def verificar():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
